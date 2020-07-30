7214a1a04460ed0e9963087c95a2dabfeddb6f2d
TATIANE <tatiane@tatianepedrelli.com.br>
2020-07-30 17:00:24 -0300

    added exercise 13

---
+++ Exercise13.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace ProwayExercises
+{
+    class ExerciseThirteen
+    {
+        public static void ShowBiggestValue(string[] args){
+            Console.WriteLine("\nExercício 13.");
+            Console.WriteLine("Ler 10 valores e determinar o maior entre eles.\n");
+
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine($"Informe o {i + 1}º valor: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int biggestNumber = 0;
+
+            foreach (int number in numbers) {
+                if (number > biggestNumber) biggestNumber = number;
+            }
+
+            Console.WriteLine($"\nO maior número entre os dez é o {biggestNumber}\n");
+            Console.ReadLine();
+        }
+    }
+}
