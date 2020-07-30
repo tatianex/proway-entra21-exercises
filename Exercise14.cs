9f3df0add0feabe0d48fb39d7f9f80a626e993e3
TATIANE <tatiane@tatianepedrelli.com.br>
2020-07-30 17:00:53 -0300

    added exercise 14

---
+++ Exercise14.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ProwayExercises
+{
+    class ExerciseFourteen
+    {
+        public static void SortValues(string[] args){
+            Console.WriteLine("\nExercício 14.");
+            Console.WriteLine("Ler três valores e colocá-los em ordem\n");
+
+            int[] numbers = new int[3];
+
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine($"Informe o {i + 1}º valor: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+
+            Console.WriteLine("\n");
+
+            foreach (int number in numbers) {
+                Console.Write($"{number}, ");
+            }
+
+            Console.WriteLine("\n");
+            Console.ReadLine();
+        }
+    }
+}
