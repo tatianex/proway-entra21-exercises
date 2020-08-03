c423d3a6d7eb62c1e8bde954c950099ab6460ee0
TATIANE <tatiane@tatianepedrelli.com.br>
2020-08-03 20:36:43 -0300

    fixed exercise 3

--- Exercise3.cs
+++ Exercise3.cs
@@ -9,10 +9,10 @@ namespace ProwayExercises
             Console.WriteLine("\nExercício 3\n");
             Console.WriteLine("Imprimir todos os números ímpares menores de 200");
 
-            for(int i = 0; i < 201; i += 2){
+            for(int i = 1; i < 200; i += 2){
                Console.WriteLine(i);
             }
-            
+
             Console.ReadLine();
         }
     }
