7c54809c5282716b07dd0bc1601cb869f2a8b45d
TATIANE <tatiane@tatianepedrelli.com.br>
2020-08-03 20:40:11 -0300

    fixed exercise 2

--- Exercise2.cs
+++ Exercise2.cs
@@ -6,15 +6,15 @@ namespace ProwayExercises
     {
         public static void SumIntNumbers(string[] args)
         {
-
             Console.WriteLine("\nExercício 2");
             Console.WriteLine("Imprimir a soma dos números inteiros de 1 a 100");
             int sum = 0;
+
             for(int x = 1; x < 101; x++){
                 sum += x;
             }
+            
             Console.WriteLine($"A soma dos números inteiros de 1 a 100 é de: {sum}");
-            Console.WriteLine("******************* \nFim do exercício 2\n");
             Console.ReadLine();
         }
     }
