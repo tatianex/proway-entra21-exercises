cbfc1b16622aba100cce8900f784f3eaac4f02b9
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2020-09-15 20:06:41 -0300

    fixed exercise 8

--- Exercise8.cs
+++ Exercise8.cs
@@ -14,10 +14,13 @@ namespace ProwayExercises
             Console.WriteLine("Informe o segundo número");
             int n2 = Convert.ToInt32(Console.ReadLine());
 
-            if (n1 % n2  == 0) {
-                Console.WriteLine($"{n1} é múltiplo de {n2}\n");
+            try {
+                if (n1 % n2  == 0) Console.WriteLine($"{n1} é múltiplo de {n2}\n");
+                else Console.WriteLine($"{n1} não é múltiplo de {n2}\n");
             }
-            else Console.WriteLine($"{n1} não é múltiplo de {n2}\n");
+            catch {
+                Console.WriteLine("Divisão por zero, ou formato inválido. Tente novamente.");
+            }
 
             Console.ReadLine();
         }
